312e4198b3aef4fba3bc930a885611a2c2ac6753
Gr3p <eddy.dejana1991@gmail.com>
2020-02-21 11:53:14 -0900

    update welcome msg

--- util/welcome.js
+++ util/welcome.js
@@ -8,7 +8,11 @@ function welcome() {
         '\n' +
         '\t\t\t\t\t\t\t\t\tby Gr3p' +
         '\n' +
+        '\n' +
+        '\n' +
+        '[!] legal disclaimer: Usage of this tool for scanning targets without prior mutual consent is illegal. It is the end user\'s responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program'
         '\n';
+
 }
 
 module.exports = welcome;
