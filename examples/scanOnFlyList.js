5668efdf9c473468c5865404f96bfb1ca3da0a5a
gr3p1p3 <gr3pp1ng4ll@gmail.com>
2020-03-27 12:27:38 -0800

    add proxy example

---
+++ examples/scanOnFlyList.js
@@ -0,0 +1,20 @@
+/**
+ * Executing a simple Scan using list as array
+ * */
+
+const dirWalker = require('../index');
+
+(async function scan() {
+    const config = {
+        host: 'http://testphp.vulnweb.com/',
+        list: ['AJAX', 'Templates'], //dont use any file but own list of strings
+        proxy: 'http://127.0.0.1:9080/', //using local proxy
+        asyncRequests: true
+    };
+
+    const result = await dirWalker.launch(config);
+    console.log('\nFOUNDS:', result.founds.length, '/', result.sent);
+    if (result.founds.length) {
+        console.log('=>', result.founds.map((r) => (r.target)));
+    }
+})();
